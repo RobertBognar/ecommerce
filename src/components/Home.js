126e4fce3631c40c686329244c811c8363aa1edd
RobertBognar <robertbognar2@gmail.com>
2021-11-09 12:13:13 +0100

    styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,37 @@
 import React from 'react';
+import photo1 from '../images/photo1.jpg';
+import photo2 from '../images/photo2.jpg';
+import photo3 from '../images/photo3.jpg';
+import photo4 from '../images/photo4.jpg';
 
-function Home() {
+const Home = () => {
     return (
-        <header>
-            <div className="header-overlay"></div>
-            <nav>
-                <h2>Shop</h2>
-                <ul>
-                    {/* <li><a href="#">Home</a></li> */}
-                    {/* <li><a href="#">About</a></li>
-                    <li className="cart"><a href="#">Cart 🛒</a></li> */}
-                </ul>
-            </nav>
-        </header>
+        <div className="container">
+            <div className="image">
+                <img src={photo1} alt="black hoodie" />
+                <h3>Black Hoodie</h3>
+                <h3>$20,00</h3>
+                <a className="addToCart cart1" href="#">Add To Cart</a>
+            </div>
+            <div className="image">
+                <img src={photo2} alt="brown hoodie" />
+                <h3>Brown Hoodie</h3>
+                <h3>$30,00</h3>
+                <a className="addToCart cart2" href="#">Add To Cart</a>
+            </div>
+            <div className="image">
+                <img src={photo3} alt="blue hoodie" />
+                <h3>Royal Blue Hoodie</h3>
+                <h3>$40,00</h3>
+                <a className="addToCart cart3" href="#">Add To Cart</a>
+            </div>
+            <div className="image">
+                <img src={photo4} alt="pink hoodie" />
+                <h3>Pink Hoodie</h3>
+                <h3>$60,00</h3>
+                <a className="addToCart cart4" href="#">Add To Cart</a>
+            </div>
+        </div>
     );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <header>
+            <div className="header-overlay"></div>
+            <nav>
+                <h2>Shop</h2>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li className="cart"><a href="#">Cart 🛒</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
