3d71364ad3a2c196d61b9393ecf57c37551d890e
RobertBognar <robertbognar2@gmail.com>
2021-11-09 11:15:18 +0100

    header styles & positions, preparing files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from 'react';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
-      <nav>
-        <h2>Shop</h2>
-        <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Cart</a></li>
-        </ul>
-      </nav>
-
+      <Home />
     </div>
   );
 }
